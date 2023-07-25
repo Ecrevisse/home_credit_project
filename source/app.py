3c427ae0e6cd1ba6e84d3c1e106f1414d084bc43
romeo <romeo@selas.studio>
2023-07-26 01:27:17 +0200

    first implem of the API

---
+++ source/app.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import mlflow
+import uvicorn
+from fastapi import FastAPI
+from model import CreditModelInput, CreditModel
+
+app = FastAPI()
+model = CreditModel()
+
+
+@app.get("/")
+def index():
+    return {"message": "Hello, stranger"}
+
+
+@app.post("/predict")
+def predict_species(input: CreditModelInput):
+    return model.predict(input)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
---
+++ source/model.py
@@ -0,0 +1,64 @@
+import numpy as np
+import pandas as pd
+import mlflow
+from mlflow import MlflowClient
+from pydantic import BaseModel
+import gc
+
+
+class CreditModelInput(BaseModel):
+    client_id: int
+
+
+class CreditModel:
+    def __init__(self):
+        run_id = "0cb98077b68b4bba8ea208b79cb1d2e8"
+        logged_model = f"runs:/{run_id}/model"
+
+        client = MlflowClient()
+        run = client.get_run(run_id)
+
+        if run.info.status != "FINISHED":
+            raise Exception(f"Model with status {run.info.status}")
+        if "count_cols" not in run.data.params:
+            raise Exception(f"Wrong model, doesn't have count_cols")
+        if "threshold" not in run.data.params:
+            raise Exception(f"Wrong model, doesn't have threshold")
+
+        python_model_for_signature = mlflow.pyfunc.load_model(logged_model)
+        self.model = mlflow.lightgbm.load_model(logged_model)
+        cols = []
+        for elem in python_model_for_signature._model_meta._signature.inputs.to_dict():
+            cols.append(elem["name"])
+        self.count_cols = int(run.data.params["count_cols"])
+        self.threshold = float(run.data.params["threshold"])
+
+        print("loading data...")
+        self.data = self._load_and_prepare_data(cols)
+        print("model ready")
+
+    def _load_and_prepare_data(self, cols):
+        df = pd.read_csv("../input/cleaned_data.csv", index_col="index")
+
+        test_df = df[df["TARGET"].isnull()]
+
+        test_df = test_df.fillna(0)
+        test_df = test_df.replace([np.inf, -np.inf], 0)
+
+        cols.append("SK_ID_CURR")
+        test_df = test_df[cols]
+        return test_df
+
+    def predict(self, input: CreditModelInput):
+        X = self.data.loc[self.data["SK_ID_CURR"] == input.client_id]
+
+        if len(X) == 0:
+            raise Exception(f"Client ID {input.client_id} doesn't exist")
+        X = X.drop(["SK_ID_CURR"], axis=1)
+        pred = self.model.predict_proba(
+            X,
+            num_iteration=self.model.best_iteration_,
+        )[:, 1]
+        if len(pred) != 1:
+            raise Exception(f"An issue occurred with the prediction")
+        return 1 if (pred[0] > self.threshold) else 0
