3afb430ae51fe684ea27acd9ae18f4f28822e53e
romeo <romeo@selas.studio>
2023-08-28 16:24:33 +0200

    add unit tests

--- test/test_api.py
+++ test/test_api.py
@@ -6,5 +6,40 @@ testClient = TestClient(app)
 
 
 def test_healthcheck():
-    res = testClient.get("/")  # rajouter un /healthcheck avec un vrai check
+    res = testClient.get("/health")
     assert res.status_code == 200
+    assert res.json() == {"status": "ok"}
+
+
+def test_predict_granted():
+    res = testClient.post(
+        "/predict",
+        json={
+            "client_id": 208550,
+        },
+    )
+    assert res.status_code == 200
+    assert res.json() == [0, 0.07389793125440654, 0.15]
+
+
+def test_predict_denied():
+    res = testClient.post(
+        "/predict",
+        json={
+            "client_id": 144092,
+        },
+    )
+    assert res.status_code == 200
+    assert res.json() == [1, 0.27139346407148074, 0.15]
+
+
+def test_client_info():
+    res = testClient.post(
+        "/client_info",
+        json={
+            "client_id": 208550,
+            "client_infos": ["EXT_SOURCE_3", "PAYMENT_RATE"],
+        },
+    )
+    assert res.status_code == 200
+    assert res.json() == [[0.0, 0.0425209367449743]]
